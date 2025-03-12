9f29ac1f44f9c8f77b2d169a8afe22cd1d23219d
Mario Beltran <99507417+mario1525@users.noreply.github.com>
2025-03-12 07:42:58 -0500

    cambio de color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
 export default function Home() {
   return (
-    <div>
-
+    <div className="min-h-screen flex items-center justify-center bg-[var(--color-background)] text-[var(--color-foreground)]">
+      <div className="text-center p-6 border-2 border-[var(--color-foreground)] rounded-xl shadow-lg">
+        <h1 className="text-4xl font-bold">SGS Colombia</h1>
+        <p className="mt-4 text-lg">Estado de los chequeos.</p>
+      </div>
     </div>
   );
 }
