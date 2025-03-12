b4f14ccd4e0faf0b670db90de23f661732a19204
Mario Beltran <99507417+mario1525@users.noreply.github.com>
2025-03-12 12:57:24 +0000

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.innerHTML = `window.$zoho=window.$zoho || {}; $zoho.salesiq=$zoho.salesiq||{ready:function(){}}`;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src =
+      "https://salesiq.zohopublic.com/widget?wc=siq1b223a368a90eddef5e58636e729637f93f8d110f63a867871603095ebe2e15c";
+    script2.defer = true;
+    document.body.appendChild(script2);
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-[var(--color-background)] text-[var(--color-foreground)]">
       <div className="text-center p-6 border-2 border-[var(--color-foreground)] rounded-xl shadow-lg">
